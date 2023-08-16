039f857a1b2537f0f98129abf9778873a5db2a87
Duy-QuocDinh <dinhquocduy5@gmail.com>
2023-08-16 12:14:09 +0700

    test

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,7 +1,7 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=thahvi123
+export POSTGRES_PASSWORD=thanhvi123
 export POSTGRES_HOST=database-1.c7htfbrpwvlb.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=database-1
 export AWS_BUCKET=myappudacity
